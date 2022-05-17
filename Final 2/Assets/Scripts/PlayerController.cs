40b9f303c317bdae5577708ebfbe6e17289342a5
Munoz <91285396+fredluva@users.noreply.github.com>
2022-05-17 12:28:41 -0700

    added gag and hugh can now do movent nice ly

--- Final 2/Assets/Scripts/PlayerController.cs
+++ Final 2/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 5;
+    Rigidbody2D rb2D;
+    float horizontal;
+    
+
+    Animator ani;
     // Start is called before the first frame update
     void Start()
     {
-        
+        ani = GetComponent<Animator>();
+        rb2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        horizontal = Input.GetAxis("Horizontal");
         
+
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 position = rb2D.position;
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+
+        rb2D.MovePosition(position);
     }
 }
